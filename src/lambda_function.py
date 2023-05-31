9bfecd8d6b9228321c547d02dabf003d7203d634
Obiana Ifeanyi <Ifeanyiobiana@gmail.com>
2023-05-31 23:49:17 +0100

    Update lambda_function.py

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,3 +1,4 @@
+import json
 import boto3
 
 # Create an EC2 client
@@ -5,6 +6,23 @@ ec2 = boto3.client('ec2')
 
 # Define the Lambda function handler
 def lambda_handler(event, context):
+    
+    # Retrieve the bucket name from the event
+    bucket_name = event['Records'][0]['s3']['bucket']['name']
+    timestamp = event["Records"][0]["eventTime"]
+    s3_key = event["Records"][0]["s3"]["object"]["key"]
+    s3_data_size = event["Records"][0]["s3"]["object"]["size"]
+    ip_address = event["Records"][0]["requestParameters"]["sourceIPAddress"]
+    event_type = event["Records"][0]["eventName"]
+    owner_id = event["Records"][0]["s3"]["bucket"]["ownerIdentity"]["principalId"]
+    
+    # Print the bucket name
+    print("Bucket Name:", bucket_name)
+    print("s3_key:", s3_key)
+    print("s3_size:", s3_data_size)
+    print("event_type:", event_type)
+    print("owner_id:", owner_id)
+    
     # Define EC2 instance parameters
     instance_params = {
         'ImageId': 'ami-xxxxxxxx',  # Specify the desired Amazon Machine Image (AMI) ID
@@ -20,3 +38,6 @@ def lambda_handler(event, context):
         print(f"EC2 instance created with ID: {instance_id}")
     except Exception as e:
         print(f"Error creating EC2 instance: {str(e)}")
+        
+        
+    
